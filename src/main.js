8546cb6cfcf110d65a382b655dedb84f00b91867
bzz-debug <ben.sumner@hotmail.com>
2025-07-16 21:20:08 +0100

    adding custom potrait images

--- src/main.js
+++ src/main.js
@@ -3,39 +3,6 @@ import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
 
-document.querySelector("#app").innerHTML = `
-  
-      <header>
-        <h1>Pocket Pebble Shop</h1>
-
-        <h3>Welcome to Pocket Pebble Shop</h3>
-        <h3>
-          We create custom potraits for you, featuring drawings of pets,
-          friends, cars, anything you like!
-        </h3>
-        <h3>We also have a collection of pre-made pebbles you can purchase.</h3>
-        <h3>
-          
-          <a href="#custom-pebbles">browse our collection</a>
-        </h3>
-      </header>
-      <main>
-        <section id="custom-pebbles">
-        <h2 class="section-title">Custom Pebbles</h2>
-        <p>ushuioshfsuoio</p>
-        </section>
-        <section id="premade-pebbles">
-        <h2 class="section-title">Pre Made Pebbles</h2>
-         <p>ushuioshfsuoio</p>
-        </section>
-        <section id="about-section">
-        <h2 class="section-title">About Pocket Pebble Shop</h2>
-        </section>
-        <section id="contact-form">
-        <h2 class="section-title">Contact us to make a purchase!</h2></section>
-      </main>
-      <footer>copyright stuff blah blah blah</footer>
-    
-`;
+document.querySelector("#app").innerHTML = ``;
 
 setupCounter(document.querySelector("#counter"));
