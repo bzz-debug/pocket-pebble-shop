07e695067141d5860a5f32ea3780ea7b0e82df5a
bzz-debug <ben.sumner@hotmail.com>
2025-08-04 14:50:00 +0100

    adding mostly completed paypal feature, however still cannot work out how to capture the purchase. All steps appear to work but the balance does not change

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+//import dotenv?? - 13:53 on video
+import "./services/paypal.js";
+import { capturePayment, createOrder } from "./services/paypal.js";
+
+const app = express();
+
+app.listen(8080, () => console.log("server running"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/pay", (req, res) => {
+  createOrder()
+    .then((url) => {
+      res.redirect(url);
+    })
+    .catch((error) => {
+      res.send("Error: " + error);
+    });
+});
+
+app.get("/complete-order", (req, res) => {
+  return capturePayment(req.query.token)
+    .then(result)
+    .catch((error) => {
+      res.send("Error: " + error);
+    });
+});
+
+app.get("/cancel-order", (req, res) => {
+  res.send("order canceled");
+});
+
+//cant get the payment to actually go through and affect the balance on paypal sandbox
---
+++ services/paypal.js
@@ -0,0 +1,95 @@
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+function generateAccessToken() {
+  return axios({
+    url: process.env.PAYPAL_BASE_URL + "/v1/oauth2/token",
+    method: "post",
+    data: "grant_type=client_credentials",
+    auth: {
+      username: process.env.PAYPAL_CLIENT_ID,
+      password: process.env.PAYPAL_SECRET,
+    },
+  }).then((response) => {
+    console.log(response.data.access_token);
+    return response.data.access_token;
+  });
+  //make sure to error handle here
+}
+
+export const createOrder = () => {
+  return generateAccessToken()
+    .then((accessToken) => {
+      return axios({
+        url: process.env.PAYPAL_BASE_URL + "/v2/checkout/orders",
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + accessToken,
+        },
+        data: JSON.stringify({
+          intent: "CAPTURE",
+          purchase_units: [
+            {
+              items: [
+                {
+                  name: "robin pebble",
+                  description: "when robins appear, loved ones are near",
+                  quantity: 1,
+                  unit_amount: {
+                    currency_code: "GBP",
+                    value: "5",
+                  },
+                },
+              ],
+              amount: {
+                currency_code: "GBP",
+                value: "5",
+                breakdown: {
+                  item_total: {
+                    currency_code: "GBP",
+                    value: "5",
+                  },
+                },
+              },
+            },
+          ],
+          application_context: {
+            return_url: process.env.BASE_URL + "/complete-order",
+            cancel_url: process.env.BASE_URL + "/cancel-order",
+            user_action: "PAY_NOW",
+            brand_name: "Pocket Pebbles",
+            //can I just have the endpoint here, like I did in the html file with
+          },
+        }),
+      });
+    })
+    .then((response) => {
+      //   console.log(response.data);
+      return response.data.links.find((link) => link.rel === "approve").href;
+    });
+};
+
+export const capturePayment = (orderId) => {
+  console.log("Capturing payment: ", orderId);
+  return generateAccessToken()
+    .then((accessToken) => {
+      return axios({
+        url:
+          process.env.PAYPAL_BASE_URL +
+          `/v2/checkout/orders/${orderId}/capture`,
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + accessToken,
+        },
+      });
+    })
+    .then((response) => {
+      console.log("Capture Response: ", response.data);
+      return response.data;
+    });
+};
+
+// createOrder().then((result) => console.log(result));
